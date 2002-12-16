f9d754801e1f962fbdad7d65c819fec9dee47f24
anthony <anthony@193a2c6b-7746-0410-b024-e06b4296b71a>
2002-12-16 04:44:03 +0000

    Added some simple report parsing to the smoketest, as well as fixing a couple of bugs. Once the parsing's done, I can start in earnest on the AI/testing side of things.

--- smoketest/bot.py
+++ smoketest/bot.py
@@ -1,4 +1,4 @@
-import random
+import random, re
 
 directions = ['n','s','se','sw','ne','nw']
 
@@ -6,6 +6,163 @@ def generateturn(report, template):
     """Given a report and a template, return a set of orders as a string."""
     firstunit = 'no'
     orders = ''
+    
+    # Need to concatenate lines in the report if they don't have a . on the end!
+    tempreport = []
+    temp = ''
+    for line in report:
+        if len(line) > 2 and line[-2] == '.': # -2 since we'll have \n's at the end
+            withoutspaces = re.sub('\s+',' ',temp+line)
+            tempreport.append(withoutspaces)
+            temp=''
+        else:
+            temp += line[:-1] + ' '*(temp!='')
+    report = tempreport
+    
+    # Here we parse the report and template lists to get out relevant info.
+    # First, set up some regexps to get out relevant info.
+    
+    regionpattern = re.compile(r'''(\w+)  # initial region
+\s+                     # then a space
+\((\d+),(\d+)\)         # x,y coords
+\s+in\s+(\w+)           # region name
+,\s+(\d+)\s+peasants\s+ # number of peasants
+\((.*)\),\s+            # racial type
+\$(\d+)\.               # max tax
+''', re.VERBOSE)
+
+    citypattern = re.compile(r'''(\w+)  # initial region
+\s+                     # then a space
+\((\d+),(\d+)\)         # x,y coords
+\s+in\s+(\w+)           # region name
+,\s+contains\s+(.*)\s+\[(\w+)\]  # city details
+,\s+(\d+)\s+peasants\s+ # number of peasants
+\((.*)\),\s+            # racial type
+\$(\d+)\.               # max tax
+''', re.VERBOSE)
+
+
+    # Now, go through each region and pull out info
+    # Might be easier to do with split/string stuff, too
+    region = {}
+    units={}
+    dictstring=None
+    for line in report:
+        if line.startswith('--'):
+            #the previous line must be the start of a region
+            lineindex = report.index(line)
+            print "Found a region:",report[lineindex-1]
+            
+        wibble = regionpattern.search(line[:-1])
+        if wibble != None:
+            #print line
+            #print "***MATCH***: ",wibble.groups()
+            
+            thisregion = {}
+            thisregion['type']=wibble.groups()[0]
+            thisregion['x']=wibble.groups()[1]
+            thisregion['y']=wibble.groups()[2]
+            thisregion['province']=wibble.groups()[3]
+            thisregion['settlename']=None
+            thisregion['settletype']=None
+            thisregion['pop']=wibble.groups()[4]
+            thisregion['race']=wibble.groups()[5]
+            thisregion['maxtax']=wibble.groups()[6]
+            
+            dictstring = wibble.groups()[1]+','+wibble.groups()[2]
+            region[dictstring]=thisregion
+            units[dictstring]=[]
+            
+        wibble2 = citypattern.search(line[:-1])
+        if wibble2 != None:
+            #print line
+            #print "***MATCH***: ",wibble2.groups()
+            
+            thisregion = {}
+            thisregion['type']=wibble2.groups()[0]
+            thisregion['x']=wibble2.groups()[1]
+            thisregion['y']=wibble2.groups()[2]
+            thisregion['province']=wibble2.groups()[3]
+            thisregion['settlename']=wibble2.groups()[4]
+            thisregion['settletype']=wibble2.groups()[5]
+            thisregion['pop']=wibble2.groups()[6]
+            thisregion['race']=wibble2.groups()[7]
+            thisregion['maxtax']=wibble2.groups()[8]
+            
+            dictstring = wibble2.groups()[1]+','+wibble2.groups()[2]
+            region[dictstring]=thisregion
+            units[dictstring]=[]
+        
+        # Now for the units...
+        if line.startswith('* ') or line.startswith('- '):
+            unit = {}
+            unit['skills']=[]
+            unit['items']=[]
+            
+            if line.startswith('*'):
+                unit['mine']='yes'
+            else:
+                unit['mine']='no'
+            
+            # If the next line begins with two spaces, 
+            # we know we've split a line at the wrong point
+            lineindex = report.index(line)+1
+            while lineindex<len(report) and report[lineindex].startswith(' '):
+                #print "joined up some lines"
+                line += report[lineindex]
+                lineindex += 1
+                
+            #print "***MATCH***: found a unit:"
+            #print line
+            
+            # what about comments? split the comment off first,
+            # since we might have ., in the comment
+            if ';' in line:
+                line, ignored = line.split(';')
+            
+            temp = line.split('.')
+            
+            if len(temp)==5:  # Normal unit
+                unit['main'], unit['weight'], unit['capacity'], unit['skillbit'], ignored = line.split('.')
+                unit['combatspell']=unit['spells']=None
+            elif len(temp)==2:  #Not one of our units
+                unit['main'], ignored = line.split('.')
+                unit['combatspell']=unit['spells']=unit['weight']=unit['capacity']=unit['skillbit']=None
+            elif len(temp)==7:  # Mage unit
+                unit['main'], unit['weight'], unit['capacity'], unit['skillbit'], unit['combatspell'], unit['spells'], ignored = line.split('.')
+            else:
+                #print "Found a wacky unit:",line
+                continue
+                
+            itemlist=unit['main'].split(',')
+            #print itemlist
+            for thingo in itemlist:
+                if '[' in thingo:
+                    #print thingo, "is an item!"
+                    unit['items'].append(thingo)
+            
+            if unit['skillbit'] != None:
+                itemlist=unit['skillbit'].split(',')
+                #print itemlist
+                for thingo in itemlist:
+                    if '[' in thingo:
+                        #print thingo, "is a skill!"
+                        unit['skills'].append(thingo)
+            
+            # Need the region dictstring at this point... should be ok
+            if dictstring==None: #we're in the nexus
+                if 'nexus' not in units.keys():
+                    units['nexus']=[]
+                units['nexus'].append(unit)
+            else:
+                units[dictstring].append(unit)
+            #print
+    
+    #print region
+    #print
+    #print units
+    
+    
     # Read each line of the template.
     for line in template:
         if line.startswith('#end'):  #end of file, or end of orders
@@ -26,7 +183,7 @@ def generateturn(report, template):
                 firstunit = 'found'
             
             # for a test, we'll move around randomly
-            if random.random() >= 0.8:
+            if random.random() >= 0.5:
                 temp = int(random.random()*len(directions))
                 temp2 = int(random.random()*len(directions))
                 orders += "move "+directions[temp]+" "+directions[temp2]+"\n"
--- smoketest/smoketest.py
+++ smoketest/smoketest.py
@@ -32,8 +32,6 @@ from bot import generateturn
 
 args = sys.argv
 
-#print "Arguments are:", args
-
 if len(args) == 1 or "--help" in args:
     # print usage message and quit
     print "You need to feed smoketest some arguments to tell it what to do:"
@@ -97,8 +95,7 @@ if not ("--test" in args or "--all" in args):
     print "Skipping testing ... like a real man"
     sys.exit(0)
 
-# we do our testing here
-
+# Now, we test
 # start by preparing the testing directory
 os.system('rm -r testing')
 os.mkdir("testing")
@@ -109,10 +106,6 @@ os.system('cp ../../'+gamename+'/'+gamename+' .')
 os.system('cp ../../'+gamename+'/html/'+gamename+'.html .')
 
 # now run some tests...
-print "Bah! Testing is for sissies!"
-#time.sleep(4)
-print "Oh alright, I'll run some tests then..."
-
 print "You'll need to feed in the values for the size of the game map"
 os.system('./'+gamename+' new')
 
@@ -123,14 +116,12 @@ if os.access('players.out', os.F_OK) != 1:
     print "There was some problem with writing the players.out file"
     sys.exit(3)
 
-print "See? I told you everything was ok!"
-print "Let's run some turns"
+print "Running "+str(numturns)+" turns.."
 
 os.rename('game.out','game.in')
 os.rename('players.out','players.in')
 
 # 7. add players into the players.in file
-
 players = []
 for index in range(numplayers):
     players.append('Player '+str(index+1))
@@ -145,68 +136,55 @@ playersfile.close()
 # Now we can start looping and running turns.
 thisturn = 0
 
-# directions = ['n','s','se','sw','ne','nw']
-
 while thisturn <= numturns:
     print "Turn",thisturn,"of",numturns
     
-    # 8a. Move turn reports and templates
+    # Move turn reports and templates
     if os.access(str(thisturn), os.F_OK) == 1:
         os.system('rm -r '+str(thisturn))    
     os.system('mkdir '+str(thisturn))
     os.system('mv report.* '+str(thisturn))
     os.system('mv template.* '+str(thisturn))
     
-    # 8c. run a turn
-    os.system('./'+gamename+' run')
+    # run a turn
+    os.system('./'+gamename+' run > /dev/null')
     
     # 9. move the old order files into a directory
     os.system('mv orders.* '+str(thisturn))
     
-    # 10. parse reports
-    
-    # We won't parse reports for the moment, we'll just move around randomly
-    # and buy stuff (ie peas)
-    
     # 11. create new orders
-    
     for index in range(numplayers):
-        print "Writing orders for player",index+1
+        print "Player "+str(index+1)+":"
         
         if (os.access('report.'+str(index+3), os.F_OK) != 1 or 
            os.access('template.'+str(index+3), os.F_OK) != 1):
             print "The player has been eliminated!"
             continue
 
-        # 8b. update the players.in file so that the players don't time out.
-        #     ie, Set the LastOrders field
+        # update the players.in file so that the players don't time out.
+        # ie, Set the LastOrders field
         setplayerinfo(index+3, 'lastorders', str(thisturn))
         
-        print "opening template for player",index+1,": template."+str(index+3)
+        #print "opening template for player",index+1,": template."+str(index+3)
         templatefile = open('template.'+str(index+3), 'r') # readonly
         template = templatefile.readlines()
         
-        print "opening report for player",index+1,": report."+str(index+3)
+        #print "opening report for player",index+1,": report."+str(index+3)
         reportfile = open('report.'+str(index+3), 'r') # readonly
         report = reportfile.readlines()
         
         orders = generateturn(report, template)
         
-        print "writing orders for player",index+1,": orders."+str(index+3)
+        #print "writing orders for player",index+1,": orders."+str(index+3)
         orderfile = open('orders.'+str(index+3), 'w+') # overwrite
         orderfile.write(orders)
         orderfile.close()
     
     # 13. move old reports
-    
     os.system('mv game.in '+str(thisturn))
     os.system('mv game.out game.in')
     os.system('mv players.in '+str(thisturn))
     os.system('mv players.out players.in')
     
     thisturn += 1
-    
-    # os.system('mv times.* check.* '+str(thisturn))
-    
-    # 14. unless we've run enough turns, jump back to step 8
 
