02cf5e1330a212c7fd7bdfb3267e842c0f839b72
Enno Rehling <enno.rehling@gmail.com>
2015-11-13 13:56:33 +0100

    fix a possible uninitialized use of reg (if no match is made in first loop)

--- standard/extra.cpp
+++ standard/extra.cpp
@@ -97,25 +97,21 @@ int Game::SetupFaction( Faction *pFac )
 
 Faction *Game::CheckVictory()
 {
-	ARegion *reg;
 	forlist(&regions) {
 		ARegion *r = (ARegion *)elem;
 		forlist(&r->objects) {
 			Object *obj = (Object *)elem;
 			if (obj->type != O_BKEEP) continue;
 			if (obj->units.Num()) return NULL;
-			reg = r;
-			break;
-		}
-	}
-	{
-		// Now see find the first faction guarding the region
-		forlist(&reg->objects) {
-			Object *o = reg->GetDummy();
-			forlist(&o->units) {
-				Unit *u = (Unit *)elem;
-				if (u->guard == GUARD_GUARD) return u->faction;
+			// Now see find the first faction guarding the region
+			forlist(&reg->objects) {
+				Object *o = reg->GetDummy();
+				forlist(&o->units) {
+					Unit *u = (Unit *)elem;
+					if (u->guard == GUARD_GUARD) return u->faction;
+				}
 			}
+			break;
 		}
 	}
 	return NULL;
