15e267ce947984e3ae347f639b5a3d8b74b50c18
Valdis Zobēla <valdis@zobela.eu>
2022-01-24 19:05:31 +0200

    math consts

--- mapgen.cpp
+++ mapgen.cpp
@@ -8,8 +8,16 @@
 #include <iostream>
 #include <string>
 
-#define _USE_MATH_DEFINES
+#define __USE_MISC
 #include <cmath>
+
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
+#ifndef M_E
+    #define M_E 2.7182818284590452354
+#endif
 
 // #define T_UNKNOWN        0
 // #define T_LAKE           2
