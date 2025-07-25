89ca816c86a8ffd40781d1cae0def98bde4768ff
Enno Rehling <enno.rehling@gmail.com>
2025-07-25 19:32:39 +0200

    explicitly run registered test suites

--- unittest/main.cpp
+++ unittest/main.cpp
@@ -1,7 +1,11 @@
 #include "external/boost/ut.hpp"
 
+namespace ut = boost::ut;
+
 // This is the driver for the unit test suite.   The tests
 // are defined in the individual files (suites) in the tests
 // directory, and the framework registers and executes them
 // automatically.
-int main() { }
+int main() {
+    return ut::cfg<>.run();
+ }
