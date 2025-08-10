2950b4b51036ff3e66d8176e29235345aeab8bba
Enno Rehling <enno.rehling@gmail.com>
2025-08-10 15:33:56 +0200

    make snapshot tests fallback to cmake binaries
    also address shellcheck warnings:
    * add quotes to prevent globbing and word splitting
    * Check exit codes directly, don't use test + $?

--- snapshot-tests/run-game-snapshots.sh
+++ snapshot-tests/run-game-snapshots.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
-game=$1
-if [[ $game = "" || $game = "standard" ]]; then
+game="$1"
+if [[ "$game" = "" || "$game" = "standard" ]]; then
   game=standard
   executable=../standard/standard
   turndir=turns
 else
-  executable=../$game/$game
-  turndir=${game}_turns
+  executable="../$game/$game"
+  turndir="${game}_turns"
 fi
 
-if [[ ! -f $executable ]]; then
+[ -e "${executable}" ] || executable="../build/$game"
+
+if [[ ! -e "${executable}" ]]; then
   echo "Please build the $game executable before running the snapshot tests.  Test failed."
   exit 1
 fi
 
-cp $executable ./$game
-chmod +x ./$game
+cp "${executable}" "./${game}"
+chmod +x "./$game"
 
-if [[ ! -f $turndir/turn ]]; then
+if [[ ! -f "$turndir/turn" ]]; then
   echo "No turns defined for $game.  Test failed."
   exit 1
 fi
 
 lastTurn=$(<"$turndir/turn")
 
-for turn in $(seq 0 $lastTurn)
+for turn in $(seq 0 "$lastTurn")
 do
   echo -n "Replaying $game turn $turn..."
 
-  if [[ ! -d $turndir/turn_$turn ]]; then
+  if [[ ! -d "$turndir/turn_$turn" ]]; then
     echo "turn $turn missing. -- Test failed."
     exit 1
   fi
 
-  cp -f $turndir/turn_$turn/game.in game.in
-  cp -f $turndir/turn_$turn/players.in players.in
-  cp -f $turndir/turn_$turn/orders.3 orders.3
+  cp -f "$turndir/turn_$turn/game.in" game.in
+  cp -f "$turndir/turn_$turn/players.in" players.in
+  cp -f "$turndir/turn_$turn/orders.3" orders.3
 
-  ./$game run &> engine-output.txt
-  if [[ $? != 0 ]]; then
+  if ! "./$game" run &> engine-output.txt ; then
     echo "executable crashed. -- Test failed."
     cat engine-output.txt
     rm -f game.*
@@ -49,28 +50,27 @@ do
     rm -f template.*
     rm -f report.*
     rm -f engine-output.txt
-    rm -r ./$game
+    rm -r "./$game"
     exit 1
   fi
     
-  mkdir -p output/turn_$turn
-  mv game.* output/turn_$turn
-  mv players.* output/turn_$turn
+  mkdir -p "output/turn_$turn"
+  mv game.* "output/turn_$turn"
+  mv players.* "output/turn_$turn"
   if [[ $(shopt -s nullglob; set -- times.*; echo $#) -ge 1 ]]; then
-    mv times.* output/turn_$turn
+    mv times.* "output/turn_$turn"
   fi
-  mv orders.* output/turn_$turn
-  mv template.* output/turn_$turn
-  mv report.* output/turn_$turn
-  mv engine-output.txt output/turn_$turn
+  mv orders.* "output/turn_$turn"
+  mv template.* "output/turn_$turn"
+  mv report.* "output/turn_$turn"
+  mv engine-output.txt "output/turn_$turn"
 
-  diff -ur $turndir/turn_$turn output/turn_$turn &> turn-difference.txt
-  if [[ $? != 0 ]]; then
+  if ! diff -ur "$turndir/turn_$turn" "output/turn_$turn" &> turn-difference.txt ; then
     echo "output differed. -- Test failed."
     cat turn-difference.txt
     rm -f turn-difference.txt
     rm -rf ./output
-    rm -r ./$game
+    rm -r "./$game"
     exit 1
   fi
 
@@ -79,5 +79,5 @@ do
 done
 
 rm -rf ./output
-rm -r ./$game
+rm -r "./$game"
 exit 0
--- snapshot-tests/run-rules-snapshot.sh
+++ snapshot-tests/run-rules-snapshot.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
 
-game=$1
-if [[ $game = "" || $game = "standard" ]]; then
+cleanup()
+{
+  game="$1"
+  rm -f "${game}"
+  rm -f "${game}.html"
+  rm -f "${game}_intro.html"
+  rm -f "${game}.html.strip"
+  rm -f rules-difference.txt
+  rm -f engine-output.txt
+}
+
+game="$1"
+if [[ "$game" = "" || "$game" = "standard" ]]; then
   game=standard
   executable=../standard/standard
 else
-  executable=../$game/$game
+  executable="../$game/$game"
 fi
 
-if [[ ! -f $executable ]]; then
+[ -e "${executable}" ] || executable="../build/$game"
+
+if [[ ! -f "${executable}" ]]; then
   echo "Please build the $game executable before running the rules snapshot tests.  Test failed."
   exit 1
 fi
 
-cp $executable ./$game
-chmod +x ./$game
+cp "$executable" "./$game"
+chmod +x "./$game"
 
 echo -n "Regenerating $game rules..."
 
-cp ../$game/${game}_intro.html .
-./$game genrules ${game}_intro.html ${game}.css ${game}.html &> engine-output.txt
-if [[ $? != 0 ]]; then
+cp "../$game/${game}_intro.html" .
+if ! "./$game" genrules "${game}_intro.html" "${game}.css" "${game}.html" &> engine-output.txt ; then
   echo "executable crashed. -- Test failed."
   cat engine-output.txt
-  rm -f ./engine-output.txt
-  rm -f ${game}_intro.html
-  rm -f ${game}.html
-  rm -f ./${game}
+  cleanup "${game}"
   exit 1
 fi
 
 rm -f ./engine-output.txt
 
 # Clean up the Last Change: timestamp line so that comparisons can work
-sed -e 's/Last Change:.*$/Last Change: __STRIPPED_FOR_COMPARISON__/' ${game}.html > ${game}.html.strip
+sed -e 's/Last Change:.*$/Last Change: __STRIPPED_FOR_COMPARISON__/' "${game}.html" > "${game}.html.strip"
 
-diff -ur rules/${game}.html ${game}.html.strip &> rules-difference.txt
-if [[ $? != 0 ]]; then
+if ! diff -ur "rules/${game}.html" "${game}.html.strip" &> rules-difference.txt ; then
   echo "output differed. -- Test failed."
   cat rules-difference.txt
-  rm -f rules-difference.txt
-  rm -f ${game}_intro.html
-  rm -f ${game}.html
-  rm -f ${game}.html.strip
-  rm -f ./${game}
+  cleanup "${game}"
   exit 1
 fi
 
 echo "identical. -- Test succeeded."
-rm -f rules-difference.txt
-rm -f engine-output.txt
-rm -f ${game}.html
-rm -f ${game}_intro.html
-rm -f ${game}.html.strip
-rm -f ./$game
+cleanup "${game}"
 exit 0
