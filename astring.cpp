9e5d1e8dba80b7fe7fe6d33c4e4220a6ec651ad6
Artem Trytiak <art.trityak@gmail.com>
2026-07-25 00:20:02 -1000

    Decay test disable

--- astring.cpp
+++ astring.cpp
@@ -88,10 +88,7 @@ AString::AString(const std::string & s) {
 
 AString::~AString()
 {
-	// str is ALWAYS allocated with new char[...] (see every ctor and operator=),
-	// so it must be freed with delete[]. Plain `delete str` is an
-	// alloc-dealloc-mismatch (UB) that ASan aborts on; do not "simplify" it.
-	if (str) delete[] str;
+	if (str) delete str;
 	str = NULL;
 }
 
@@ -105,7 +102,7 @@ AString::AString(const AString &s)
 AString & AString::operator=(const AString &s)
 {
 	len = s.len;
-	if (str) delete[] str;
+	if (str) delete str;
 	str = new char[len + 1];
 	strcpy(str,s.str);
 	return *this;
@@ -115,7 +112,7 @@ AString & AString::operator=(const char *c)
 {
 	len = 0;
 	if (c) len = strlen(c);
-	if (str) delete[] str;
+	if (str) delete str;
 	str = new char[len + 1];
 	if (c) strcpy(str,c);
 	return *this;
@@ -187,7 +184,7 @@ AString &AString::operator+=(const AString &s)
 	for (int j=0; j<s.len+1; j++) {
 		temp[i++] = s.str[j];
 	}
-	delete[] str;
+	delete str;
 	str = temp;
 	len = len + s.len;
 	return *this;
@@ -224,7 +221,7 @@ AString *AString::gettoken()
 			place++;
 		} else {
 			/* Unmatched "" return 0 */
-			delete[] str;
+			delete str;
 			str = new char[1];
 			len = 0;
 			str[0] = '\0';
@@ -238,7 +235,7 @@ AString *AString::gettoken()
 	}
 	buf[place2] = '\0';
 	if (place == len || str[place] == ';') {
-		delete[] str;
+		delete str;
 		str = new char[1];
 		len = 0;
 		str[0] = '\0';
@@ -253,7 +250,7 @@ AString *AString::gettoken()
 	}
 	buf2[place2] = '\0';
 	len = newlen;
-	delete[] str;
+	delete str;
 	str = buf2;
 	return new AString(buf);
 }
